f37a70ce7970bbaededb8d188d992a0e39949315
YJ <zerosix1017@gmail.com>
2024-03-29 23:22:59 +0900

    React Basic #3

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { useState } from "react";
 function App() {
     let post = "테스트 테스트";
     let [title, setTitle] = useState(['책 추천', '음식 추천', '영화 추천']);
-    let [like, setLike] = useState(0);
+    let [like, setLike] = useState([0, 0, 0]);
     let [modal, setModal] = useState(false);
+    let [modalTitle, setModalTitle] = useState(0);
 
     return (
         <div className="App">
@@ -18,27 +19,31 @@ function App() {
                 copy.sort();
                 setTitle(copy)
             }}>글 제목 정렬</button>
-            <div className="list">
-                <h4 onClick={() => {setModal(!modal)}}>{title[0]}
-                    <span onClick={() => {setLike(like+1)}}>👍</span>
-                    {like}</h4>
-                <button onClick={() => {
-                    let copy = [...title];
-                    copy[0] = '만화 추천'
-                    setTitle(copy)
-                }}>제목 바꾸기</button>
-                <p>3월 1일 발행</p>
-            </div>
-            <div className="list">
-                <h4>{title[1]}</h4>
-                <p>3월 2일 발행</p>
-            </div>
-            <div className="list">
-                <h4>{title[2]}</h4>
-                <p>3월 3일 발행</p>
-            </div>
             {
-                modal === true ? <Modal color={'skyblue'} title={title}/> : null
+                title.map((a, i) => {
+                    return (
+                        <div className="list">
+                            <h4 onClick={() => {setModal(!modal);
+                                setModalTitle(i)}}>{title[i]}
+                                <span onClick={() => {
+                                    let copy = [...like];
+                                    copy[i] = copy[i] + 1;
+                                    setLike(copy)
+                                }}>👍</span>
+                                {like[i]}</h4>
+                            <button onClick={() => {
+                                let copy = [...title];
+                                copy[0] = '만화 추천'
+                                setTitle(copy)
+                            }}>제목 바꾸기
+                            </button>
+                            <p>2월 17일 발행</p>
+                        </div>
+                    )
+                })
+            }
+            {
+                modal === true ? <Modal modalTitle={modalTitle} color={'skyblue'} title={title}/> : null
             }
         </div>
     );
@@ -47,7 +52,7 @@ function App() {
 function Modal(props) {
     return (
         <div className="modal" style={{background : props.color}}>
-            <h4>{props.title[0]}</h4>
+            <h4>{props.title[props.modalTitle]}</h4>
             <p>날짜</p>
             <p>상세내용</p>
             <button>글 수정</button>
